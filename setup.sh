6d8dbac44214faa97e342a7f76d2b88083aa9b18
tahirkhantksc <tahirkhantksc@gmail.com>
2026-02-07 01:28:53 +0500

    Create setup.sh installation script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Installation script for Breek Ultimate
+
+# Print starting message
+echo "Starting installation..."
+
+# Update package list
+sudo apt-get update
+
+# Install necessary packages
+sudo apt-get install -y package1 package2 package3
+
+# Print completion message
+echo "Installation completed!"
